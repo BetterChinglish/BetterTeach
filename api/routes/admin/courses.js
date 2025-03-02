dbfa95ce0750d71380a4f1f3ef378aff5c115db1
BetterChinglish <2199840669@qq.com>
2025-03-03 00:48:55 +0800

    新增courses路由文件

---
+++ api/routes/admin/courses.js
@@ -0,0 +1,186 @@
+
+const express = require('express');
+const router = express.Router();
+
+const { Course } = require('../../models');
+const {Op} = require("sequelize");
+
+const {
+  NotFoundError,
+  sendSuccessResponse,
+  handleFailure
+} = require('../../utils/response');
+
+// 获取课程列表
+router.get('/', async (req, res, next) => {
+  try {
+    // 获取查询参数
+    const query = req.query;
+
+    // 处理分页参数与默认值
+    const pageSize = Math.abs(parseInt(query.pageSize) || 10);
+    const currentPage = Math.abs(parseInt(query.currentPage) || 1);
+
+    // 分页, limit startOffset, pageSize
+    const startOffset = (currentPage - 1) * pageSize;
+
+    // 排序
+    const condition = {
+      order: [
+        ['id', 'ASC']
+      ],
+      limit: pageSize,
+      offset: startOffset
+    }
+
+    /*搜索部分----start*/
+    if (query.categoryId) {
+      condition.where = {
+        categoryId: {
+          [Op.eq]: query.categoryId
+        }
+      };
+    }
+
+    if (query.userId) {
+      condition.where = {
+        userId: {
+          [Op.eq]: query.userId
+        }
+      };
+    }
+
+    if (query.name) {
+      condition.where = {
+        name: {
+          [Op.like]: `%${ query.name }%`
+        }
+      };
+    }
+
+    if (query.recommended) {
+      condition.where = {
+        recommended: {
+          // 需要转布尔值
+          [Op.eq]: query.recommended === 'true'
+        }
+      };
+    }
+
+    if (query.introductory) {
+      condition.where = {
+        introductory: {
+          [Op.eq]: query.introductory === 'true'
+        }
+      };
+    }
+    /*搜索部分----end*/
+
+    // 查询
+    const { count, rows} = await Course.findAndCountAll(condition);
+
+    // 响应数据
+    const data = {
+      courses: rows,
+      pagination: {
+        pageSize,
+        currentPage,
+        total: count
+      }
+    }
+
+    sendSuccessResponse(res,'获取课程列表成功', data);
+  } catch(err) {
+    handleFailure(res, err)
+  }
+});
+
+// 获取课程详情
+router.get('/:id', async (req, res, next) => {
+  try {
+    const course = await getCourse(req);
+
+    sendSuccessResponse(res, `获取id为${req.params.id}的课程成功`, course)
+  } catch(err) {
+    handleFailure(res, err)
+  }
+});
+
+// 新增课程
+router.post('/', async (req, res, next) => {
+  try {
+    // 只接收title与content
+    const createData = filterBody(req);
+
+    const course = await Course.create(createData);
+
+    sendSuccessResponse(res, '新增课程成功', course, 201);
+
+  } catch(err) {
+    handleFailure(res, err)
+  }
+});
+
+
+// 删除课程
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const course = await getCourse(req);
+
+    await course.destroy();
+
+    sendSuccessResponse(res, `删除id为${req.params.id}的课程成功`)
+
+  } catch(err) {
+    handleFailure(res, err)
+  }
+});
+
+// 更新课程
+router.put('/:id', async (req, res, next) => {
+  try {
+    const updateData = filterBody(req);
+
+    const course = await getCourse(req);
+
+    await course.update(updateData);
+
+    sendSuccessResponse(res, `更新id为${req.params.id}的课程成功`, course)
+
+  } catch(err) {
+    handleFailure(res, err);
+  }
+});
+
+/**
+ * 公共方法：白名单过滤
+ * @param req
+ * @returns {{image: *, name, introductory: (boolean|*), userId: (number|*), categoryId: (number|*), content, recommended: (boolean|*)}}
+ */
+function filterBody(req) {
+  return {
+    categoryId: req.body.categoryId,
+    userId: req.body.userId,
+    name: req.body.name,
+    image: req.body.image,
+    recommended: req.body.recommended,
+    introductory: req.body.introductory,
+    content: req.body.content
+  };
+}
+
+
+async function getCourse(req) {
+  const { id } = req.params;
+
+  const course = await Course.findByPk(id);
+
+  if(!course) {
+    throw new NotFoundError(`Id: ${ id } 的课程未找到。`);
+  }
+
+  return course;
+}
+
+
+module.exports = router;
