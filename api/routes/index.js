ed8ac604bd4fb22ffc61cd8017e74ac6d2306012
BetterChinglish <2199840669@qq.com>
2025-08-28 11:07:07 +0800

    首页接口：查询推荐课程、人气课程、入门课程

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,60 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ title: 'Express' });
-});
+const {Course, Category, User} = require('../models');
+const {sendSuccessResponse, handleFailure} =
+  require('../utils/responses');
+
+router.get('/', async (req, res) => {
+  try {
+    // 推荐课程
+    const recommendedCourses = await Course.findAll({
+      attributes: {
+        exclude: ['CategoryId', 'UserId', 'content'],
+      },
+      include: [
+        {
+          model: Category,
+          as: 'category',
+          attributes: ['id', 'name'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['id', 'username', 'nickname', 'avatar', 'company'],
+        }
+      ],
+      where: {recommended: true},
+      order: [['id', 'DESC']],
+      limit: 10,
+    });
+
+    // 人气课程
+    const likesCourses = await Course.findAll({
+      attributes: {exclude: ['CategoryId', 'UserId', 'content']},
+      order: [['likesCount', 'desc'], ['id', 'desc']],
+      limit: 10
+    });
+
+    // 入门课程
+    const introductoryCourses = await Course.findAll({
+      attributes: { exclude: ['CategoryId', 'UserId', 'content'] },
+      where: { introductory: true },
+      order: [['id', 'desc']],
+      limit: 10
+    });
+
+    sendSuccessResponse(res, '获取首页数据成功。', {
+      recommendedCourses,
+      likesCourses,
+      introductoryCourses
+    });
+
+
+
+  } catch (error) {
+    handleFailure(res, error);
+  }
+})
 
 module.exports = router;
